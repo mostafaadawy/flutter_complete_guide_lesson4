fbe574ab9943a8afc6bd9630d6a612b8ea11f1bd
mostafaadawy <mostafa_adawy@ymail.com>
2021-11-14 10:04:55 +0200

    74-columns rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'New Shoes', 
+      amount: 89.66, 
+      date: DateTime.now(),
+      ),
+      Transaction(
+      id: 't2', 
+      title: 'Weekly Grocery', 
+      amount: 50.22, 
+      date: DateTime.now(),
+      ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
